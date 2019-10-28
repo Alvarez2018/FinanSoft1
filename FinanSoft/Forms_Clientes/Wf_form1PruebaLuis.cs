0f3ab948dfd5ada6c5b1c67bac73dbd4ceea4a0f
Luis Alvarez <alvarez201901@outlook.es>
2019-10-28 13:42:28 -0600

    Se agrego Forma1

---
+++ FinanSoft/Forms_Clientes/Wf_form1PruebaLuis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinanSoft.Forms_Clientes
+{
+    public partial class Wf_form1PruebaLuis : Form
+    {
+        public Wf_form1PruebaLuis()
+        {
+            InitializeComponent();
+        }
+    }
+}
